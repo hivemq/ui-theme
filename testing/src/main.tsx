160ac5dad4070f00bd1340750c1ea04c9bbedea6
Andreas Schujkow <andreas.schujkow@hivemq.com>
2024-03-13 13:01:33 +0100

    chore: update export management

--- testing/src/main.tsx
+++ testing/src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { theme, } from '../../theme/dist/index.es.js'
+
+import { theme, } from '../../theme/src/main.ts'
+
 import { ChakraBaseProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
