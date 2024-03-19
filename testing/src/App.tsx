d3de257ec6cb0165364514216c60b216a39f9412
Andreas Schujkow <andreas.schujkow@hivemq.com>
2024-03-19 14:39:50 +0100

    refactor: change file format

--- testing/src/App.tsx
+++ testing/src/App.tsx
@@ -1,8 +1,8 @@
 import { theme } from '../../theme/src/main'
 import { ChakraBaseProvider, Box } from '@chakra-ui/react'
 
-import { Buttons } from '@/views/Buttons'
-import { Headings } from '@/views/Headings'
+import { Buttons } from '@/views/buttons1'
+import { Headings } from '@/views/headings1'
 import { Colors } from '@/views/Colors'
 
 function App() {
--- testing/src/views/Headings.safetest.tsx
+++ testing/src/views/Headings.safetest.tsx
@@ -3,7 +3,7 @@ import { render } from 'safetest/react';
 import { ChakraBaseProvider } from '@chakra-ui/react';
 import { theme } from '@hivemq/ui-theme'
 
-import { Headings } from '@/views/Headings.tsx';
+import { Headings } from '@/views/headings1';
 
 // Whole App testing
 describe('Headings', () => {
