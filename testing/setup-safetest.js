e6398a763427cb85fd7a0eae993cc2a68196ed55
Andreas Schujkow <andreas.schujkow@hivemq.com>
2024-03-19 15:32:58 +0100

    chore: reconfigure setup

--- testing/setup-safetest.js
+++ testing/setup-safetest.js
@@ -5,7 +5,9 @@ setup({
   bootstrappedAt: require.resolve('./src/main.tsx'),
   // https://github.com/kolodny/safetest/issues/18
   matchImageSnapshotOptions: {
-    failureThreshold: 10000,
+    customDiffConfig: {
+      threshold: 10000,
+    },
     failureThresholdType: 'pixel',
   },
   ciOptions: {
