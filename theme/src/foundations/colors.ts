fb01ae02963ab0770a45f7ad5c1bbb78c621810e
oli-hivemq <olivier.frebourg@hivemq.com>
2024-07-16 10:39:54 +0200

    update colors

--- theme/src/foundations/colors.ts
+++ theme/src/foundations/colors.ts
@@ -1,38 +1,44 @@
+const percentageToHex = (percentage: number) => `0${Math.floor(percentage*255/100).toString(16)}`.slice(-2).toUpperCase()
+
 export const amber = {
-  700: '#4D3900',
-  600: '#8B6A08',
-  500: '#E5AF09',
-  400: '#FFC000',
-  300: '#FFCB2E',
+  700: '#3B2C00',
+  600: '#4D3900',
+  500: '#8B6A08',
+  400: '#E5AF09',
+  300: '#FFC000',
   200: '#F2E7C4',
   100: '#FCF6E3',
+  50: '#FCF9F0',
+  alpha: `#FFC000${percentageToHex(30)}`
 } as const
 
 export const blue = {
-  650: '#00162B',
+  700: '#00162B',
   600: '#001E3B',
   500: '#002F5C',
   400: '#0061BD',
   300: '#006ED6',
   200: '#BCD7F1',
   100: '#E8F3FC',
-  50: '#F2F7FC'
+  50: '#F2F7FC',
+  alpha: `#006ED6${percentageToHex(15)}`
 } as const
 
 export const green = {
-  650: '#0B210F',
+  700: '#0B210F',
   600: '#13371A',
   500: '#22592C',
   400: '#4FBF64',
   300: '#5DDF75',
   200: '#D5F8DB',
   100: '#EBFCEE',
-  50: '#F4FCF5'
+  50: '#F4FCF5',
+  alpha: `#5DDF75${percentageToHex(25)}`
 } as const
 
 export const neutrals = {
   950: '#0E0D0A',
-  900: "#1D1C17", // 171612
+  900: "#171612",
   800: '#2A2925',
   700: '#403E3A',
   600: '#706E67',
@@ -43,26 +49,41 @@ export const neutrals = {
   100: '#F0F0F0',
   50: '#FAFAFA',
   WHITE: '#FFFFFF',
+  alpha: `#706E67${percentageToHex(30)}`
 } as const
 
 export const orange = {
-  650: '#381E00',
+  700: '#381E00',
   600: '#3F2200',
   500: '#643500',
   400: '#D37100',
   300: '#FF8800',
   200: '#FDE0BE',
   100: '#FCF1E3',
-  50: '#FCF7F0'
+  50: '#FCF7F0',
+  alpha: `#FF8800${percentageToHex(20)}`
+} as const
+
+export const purple = {
+  700: '#170C3D',
+  600: '#1B0E47',
+  500: '#291766',
+  400: '#5C38D9',
+  300: '#6D47F7',
+  200: '#D9D3F0',
+  100: '#F3F0FC',
+  50: '#F8F7FC',
+  alpha: `#6D47F7${percentageToHex(15)}`
 } as const
 
 export const red = {
-  650: '#2E060C',
+  700: '#2E060C',
   600: '#400811',
   500: '#660314',
   400: '#C70C2C',
   300: '#E80E34',
   200: '#F0D5DA',
   100: '#FCF2F4',
-  50: '#FCF7F8'
+  50: '#FCF7F8',
+  alpha: `#E80E34${percentageToHex(15)}`
 } as const
