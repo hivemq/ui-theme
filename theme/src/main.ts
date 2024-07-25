f77b9a3e3002720b6ec74c6771880c4181889cbd
Andrej Schujkow <19648055+h2xd@users.noreply.github.com>
2024-07-25 11:32:55 +0200

    chore: remove eslint add biome (#12)

--- theme/src/main.ts
+++ theme/src/main.ts
@@ -1,7 +1,4 @@
-import {
-  StyleFunctionProps,
-  extendBaseTheme,
-} from '@chakra-ui/react'
+import { StyleFunctionProps, extendBaseTheme } from '@chakra-ui/react'
 import { mode } from '@chakra-ui/theme-tools'
 import { buttonTheme } from './components/button'
 import { headingTheme } from './components/headings'
@@ -12,7 +9,8 @@ import semanticColors from './style-guide/computedSemanticColors'
 export const fonts = {
   heading: "'Raleway', 'Roboto', 'Segoe UI', 'sans-serif'",
   body: "'Roboto', 'Segoe UI', 'Helvetica Neue', 'Noto Sans', 'Liberation Sans', 'Arial', 'sans-serif', 'system-ui', '-apple-system'",
-  monospace: "'IntelOne Mono', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'",
+  monospace:
+    "'IntelOne Mono', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'",
 }
 
 export const styles = {
@@ -33,7 +31,7 @@ export const theme = extendBaseTheme({
   fonts,
   colors: {
     ...colors,
-    ...semanticColors
+    ...semanticColors,
   },
   styles,
   components,
