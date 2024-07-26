6bd1c680fa43b7eb48043e2abc8eead448834984
Andreas Schujkow <andreas.schujkow@hivemq.com>
2024-07-26 15:21:57 +0200

    ci: what about now 🤔

--- testing/setup-safetest.js
+++ testing/setup-safetest.js
@@ -5,6 +5,7 @@ setup({
   bootstrappedAt: require.resolve('./src/main.tsx'),
   // https://github.com/kolodny/safetest/issues/18
   matchImageSnapshotOptions: {
+    comparisonMethod: 'ssim',
     // CI is installing a different version of chromium and ffmpeg than the one we use locally
     failureThreshold: '0.05',
     failureThresholdType: 'percent',
