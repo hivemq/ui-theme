b18e857f992d18970a8ddd4287772928b5b7369a
Andreas Schujkow <andreas.schujkow@hivemq.com>
2024-07-26 15:22:26 +0200

    ci: it's a number 👶

--- testing/setup-safetest.js
+++ testing/setup-safetest.js
@@ -7,7 +7,7 @@ setup({
   matchImageSnapshotOptions: {
     comparisonMethod: 'ssim',
     // CI is installing a different version of chromium and ffmpeg than the one we use locally
-    failureThreshold: '0.05',
+    failureThreshold: 0.05,
     failureThresholdType: 'percent',
     capture: 'viewport',
   },
