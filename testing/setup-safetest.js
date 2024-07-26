99e4f1b03cf298612bd11ae1cd171f3f94399c0e
Andreas Schujkow <andreas.schujkow@hivemq.com>
2024-07-26 15:32:02 +0200

    ci: back to the roots 🌱

--- testing/setup-safetest.js
+++ testing/setup-safetest.js
@@ -5,10 +5,11 @@ setup({
   bootstrappedAt: require.resolve('./src/main.tsx'),
   // https://github.com/kolodny/safetest/issues/18
   matchImageSnapshotOptions: {
-    comparisonMethod: 'ssim',
-    // CI is installing a different version of chromium and ffmpeg than the one we use locally
-    failureThreshold: 0.15,
-    failureThresholdType: 'percent',
+    comparisonMethod: 'pixelmatch',
+    customDiffConfig: {
+      threshold: 20000,
+    },
+    failureThresholdType: 'pixel',
   },
   ciOptions: {
     usingArtifactsDir: 'artifacts',
