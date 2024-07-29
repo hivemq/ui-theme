aa576b13a1b8f75fbc66cdd3e630c25a70f01560
Robin Atherton <robin.atherton@hivemq.com>
2024-07-29 14:47:34 +0200

    chore: added spacing to the main page for visual clarity

--- testing/src/App.tsx
+++ testing/src/App.tsx
@@ -11,34 +11,46 @@ import {Alerts} from '@/views/Alerts.tsx'
 function App() {
   return (
     <ChakraBaseProvider theme={theme}>
-      <Heading variant="h1">Colors</Heading>
-      <Box p={8}>
-        <Colors withText />
-      </Box>
-      <hr />
-      <Heading variant="h1">Semantic Colors</Heading>
-      <Box p={8}>
-        <SemanticColors withText />
-      </Box>
-      <hr />
-      <Heading variant="h1">Headings</Heading>
-      <Box p={8}>
-        <Headings />
-      </Box>
-      <hr />
-      <Heading variant="h1">Buttons</Heading>
-      <Box p={8}>
-        <Buttons />
-      </Box>
-      <hr />
-      <Heading variant="h1">Links</Heading>
-      <Box p={8}>
-        <Links />
-      </Box>
-      <hr />
-      <Heading variant="h1">Alerts</Heading>
-      <Box p={8}>
-        <Alerts />
+      <Box pl={6} pt={6}>
+        <Heading variant="h1">Colors</Heading>
+        <Box p={8}>
+          <Colors withText />
+        </Box>
+        <hr />
+        <Heading variant="h1" pt={4}>
+          Semantic Colors
+        </Heading>
+        <Box p={8}>
+          <SemanticColors withText />
+        </Box>
+        <hr />
+        <Heading variant="h1" pt={4}>
+          Headings
+        </Heading>
+        <Box p={8}>
+          <Headings />
+        </Box>
+        <hr />
+        <Heading variant="h1" pt={4}>
+          Buttons
+        </Heading>
+        <Box p={8}>
+          <Buttons />
+        </Box>
+        <hr />
+        <Heading variant="h1" pt={4}>
+          Links
+        </Heading>
+        <Box p={8}>
+          <Links />
+        </Box>
+        <hr />
+        <Heading variant="h1" pt={4}>
+          Alerts
+        </Heading>
+        <Box p={8}>
+          <Alerts />
+        </Box>
       </Box>
     </ChakraBaseProvider>
   )
