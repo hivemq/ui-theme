d1d69c85b49c18f254afe5bc6cb51e200af1c6e8
oli-hivemq <olivier.frebourg@hivemq.com>
2024-08-28 10:21:40 +0200

    improve test readability

--- theme/src/utils.spec.ts
+++ theme/src/utils.spec.ts
@@ -2,28 +2,30 @@ import { describe, expect, test } from 'vitest'
 import { chakraTokenToCssVar, percentageToHex, themedColors } from './utils'
 
 describe('utils', () => {
-  test('percentageToHex', () => {
-    expect(percentageToHex(0)).toEqual('00')
-    expect(percentageToHex(5)).toEqual('0C')
-    expect(percentageToHex(10)).toEqual('19')
-    expect(percentageToHex(15)).toEqual('26')
-    expect(percentageToHex(20)).toEqual('33')
-    expect(percentageToHex(25)).toEqual('3F')
-    expect(percentageToHex(30)).toEqual('4C')
-    expect(percentageToHex(35)).toEqual('59')
-    expect(percentageToHex(40)).toEqual('66')
-    expect(percentageToHex(45)).toEqual('72')
-    expect(percentageToHex(50)).toEqual('7F')
-    expect(percentageToHex(55)).toEqual('8C')
-    expect(percentageToHex(60)).toEqual('99')
-    expect(percentageToHex(65)).toEqual('A5')
-    expect(percentageToHex(70)).toEqual('B2')
-    expect(percentageToHex(75)).toEqual('BF')
-    expect(percentageToHex(80)).toEqual('CC')
-    expect(percentageToHex(85)).toEqual('D8')
-    expect(percentageToHex(90)).toEqual('E5')
-    expect(percentageToHex(95)).toEqual('F2')
-    expect(percentageToHex(100)).toEqual('FF')
+  test.each([
+    [0, '00'],
+    [5, '0C'],
+    [10, '19'],
+    [15, '26'],
+    [20, '33'],
+    [25, '3F'],
+    [30, '4C'],
+    [35, '59'],
+    [40, '66'],
+    [45, '72'],
+    [50, '7F'],
+    [55, '8C'],
+    [60, '99'],
+    [65, 'A5'],
+    [70, 'B2'],
+    [75, 'BF'],
+    [80, 'CC'],
+    [85, 'D8'],
+    [90, 'E5'],
+    [95, 'F2'],
+    [100, 'FF']
+  ])('transform percentage (%i) into -> hex value (%i)', (value, expected) => {
+    expect(percentageToHex(value)).toEqual(expected)
   })
 
   test('chakraTokenToCssVar', () => {
