c5950777f5041cb1f17fb39126df1a5aeb735668
oli-hivemq <olivier.frebourg@hivemq.com>
2024-09-04 17:06:40 +0200

    fix: export colors, components, fonts, semanticTokens and styles

--- testing/src/main.tsx
+++ testing/src/main.tsx
@@ -19,7 +19,7 @@ import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { bootstrap } from 'safetest/react'
-import { theme } from '../../theme/src/main'
+import theme from '../../theme/src/main'
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 const root = ReactDOM.createRoot(document.getElementById('root')!)
